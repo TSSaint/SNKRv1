85fbc788c453bc06a1a1a6b78c88066423bdc409
Thomas Santos <thomas@socialeffort.co>
2017-08-14 19:44:12 -0400

    commit

--- app.js
+++ app.js
@@ -34,14 +34,14 @@
  	
 
 
-app.use(express.static(__dirname + './public'));
+app.use(express.static(__dirname + '../public'));
 
  // sets up our views
  app.set('views', path.join(__dirname, 'views'));
  app.set('view engine', 'ejs');
 
  // sets our port
- const port = process.env.PORT || 3006;
+ const port = process.env.PORT || 3000;
  app.listen(port, () => {
      console.log(`ON PORT: ${port}`);
  });
